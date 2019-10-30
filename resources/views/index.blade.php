6d56bb8eeedba72197aad96c1f2ca05883010281
Turbanov Valeriy <tur.4000@gmail.com>
2019-10-30 16:53:23 +0200

    main page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,87 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello</h1>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit
-        amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem
-        ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Repellendus, voluptatem?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-        voluptatem?</p>
+    <div class="main">
+        <div class="main__info">
+            <h1 class="main__title">
+                <!--            Видеостудия<br> -->
+                bad7production
+            </h1>
+            <p class="main__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aspernatur deleniti earum fugiat harum illo in,
+                mollitia nisi nobis reprehenderit sit! <a href="#" class="main__link">поехали</a>
+            </p>
+        </div>
+
+        <div class="main__video">
+            video
+        </div>
+
+        <div class="main__partners">
+            партнёры
+        </div>
+    </div>
+
 @endsection
