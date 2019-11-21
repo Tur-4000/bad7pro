8c1742ea369d720175d23f6b8e265aa9aa6b4828
Turbanov Valeriy <tur.4000@gmail.com>
2019-11-21 15:32:49 +0200

    contacts

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,10 +1,40 @@
-@extends('layouts.comingsoon')
+{{--@extends('layouts.comingsoon')--}}
 
-{{--@extends('layouts.app')--}}
+@extends('layouts.app')
 
-{{--@section('title'){{ $title }}@endsection--}}
+@section('title'){{ $title }}@endsection
 
 
-{{--@section('content')--}}
-{{--    <h1>Under construction</h1>--}}
-{{--@endsection--}}
+@section('content')
+    <div class="contacts">
+        <h1 class="contacts__title">Связаться с нами</h1>
+    </div>
+
+    <div class="contacts__form form">
+        {{ Form::open(['url' => '#']) }}
+        <div class="form__element">
+            {{ Form::label('name', 'Привет, меня зовут') }}
+            {{ Form::text('name', null, ['class' => 'form-control', 'aria-describedby' => 'nameHelp']) }}
+            <p class="form__help">Укажите своё имя</p>
+        </div>
+        <div class="form__element">
+            {{ Form::label('company', 'я представляю компанию') }}
+            {{ Form::text('company', null, ['class' => 'form-control', 'aria-describedby' => 'companyHelp']) }}
+            <p class="form__help">Укажите название компании</p>
+        </div>
+        <div class="form__element">
+            {{ Form::label('request', 'я хочу') }}
+            {{ Form::textarea('request', null, ['class' => 'form-control', 'rows' => 5]) }}
+        </div>
+        <div class="form__element">
+            {{ Form::label('contact', 'со мной можно связаться') }}
+            {{ Form::text('contact', null, ['class' => 'form-control', 'aria-describedby' => 'contactHelp']) }}
+            <p class="form__help">Укажите номер телефона или электронную почту</p>
+        </div>
+        <div class="form__element">
+            {{ Form::submit('Отправить', ['class' => 'form__button']) }}
+        </div>
+        {{ Form::close() }}
+    </div>
+
+@endsection
